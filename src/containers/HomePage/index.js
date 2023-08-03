7275cac85723e8faf2994db90dbf8191e7fa40a2
hellno <hellno@users.noreply.github.com>
2023-08-03 12:17:58 +0200

    Add CommandPalette

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Home from "../../components/Home";
+import CommandPalette from "../../components/CommandPalette";
+import { useState } from 'react'
+// import { useHotkeys } from 'react-hotkeys-hook'
 
 const HomePage = () => {
-  return <Home />;
+  const [open, setOpen] = useState(false)
+  // useHotkeys(['meta+k'], () => {
+  //   console.log('hotkey called', open);
+  //   setOpen(!open);
+  // }, [open], {
+  //   enableOnContentEditable: true,
+  // })
+
+  return <>
+    <CommandPalette open={open} setOpen={setOpen} />
+    <Home />
+  </>;
 };
 
 export default HomePage;
