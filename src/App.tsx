113124376b800e33a606f215c691b383dcd3c042
hellno <hellno@users.noreply.github.com>
2023-10-15 11:59:56 +0200

    tryfix aptabase

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { disableContextMenu } from '@/common/helpers/tauri/contextMenu';
 import { init } from "@aptabase/web";
 import { RUNNING_IN_TAURI } from './common/constants/tauri';
 import { ThemeProvider } from './common/hooks/ThemeProvider';
+import { AptabaseProvider } from '@aptabase/react';
 
 import '@/globals.css';
 
@@ -13,8 +14,6 @@ export const VITE_APTABASE_KEY = import.meta.env.VITE_APTABASE_KEY
 
 if (RUNNING_IN_TAURI) {
   disableContextMenu();
-} else {
-  init(VITE_APTABASE_KEY);
 }
 
 const rootElement = document.getElementById('root');
@@ -23,6 +22,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <ThemeProvider defaultTheme="dark" storageKey="herocast-ui-theme">
-    <RouterProvider router={router} />
+    <AptabaseProvider appKey={VITE_APTABASE_KEY}>
+      <RouterProvider router={router} />
+    </AptabaseProvider>
   </ThemeProvider>
 );
