bfd9b97ac269fd7e3895dcd0bbd39933885d0388
hellno <hellno@users.noreply.github.com>
2023-10-17 19:49:49 +0200

    check

--- src/common/helpers/supabase.ts
+++ src/common/helpers/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
-export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 const options = {
   auth: {
