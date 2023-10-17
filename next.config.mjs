ffe470041ea6ceb6d62df5488f0639757a484eff
hellno <hellno@users.noreply.github.com>
2023-10-17 17:01:35 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA).
-  distDir: './dist', // Changes the build output directory to `./dist/`.
+  // distDir: './dist', // Changes the build output directory to `./dist/`.
   transpilePackages: ['react-tweet'], // https://react-tweet.vercel.app/next,
   typescript: {
     // !! WARN !!
