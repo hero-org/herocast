da6f0481ec33270505df5402044ea0d1d092d42f
hellno <hellno@users.noreply.github.com>
2023-12-03 12:21:51 +0100

    update packages

--- src/common/components/Embeds/ParagraphXyzEmbed.tsx
+++ src/common/components/Embeds/ParagraphXyzEmbed.tsx
@@ -45,7 +45,9 @@ const ParagraphXyzEmbed: React.FC<ParagraphXyzEmbedProps> = ({ url }) => {
               <div className="flex-shrink-0 justify-center flex flex-col">
                 <Avatar>
                   <AvatarImage src={data.user.avatar_url} />
-                  <AvatarFallback>{data.user.authorName || data.publication.name}</AvatarFallback>
+                  <AvatarFallback>
+                    {data.user.authorName || data.publication.name}
+                  </AvatarFallback>
                 </Avatar>
               </div>
               <div className="ml-2 flex-1 min-w-0 flex flex-col justify-center -space-y-0.5">
@@ -53,7 +55,8 @@ const ParagraphXyzEmbed: React.FC<ParagraphXyzEmbedProps> = ({ url }) => {
                   {data.user.authorName}
                 </div>
                 <div className="flex flex-row space-x-2">
-                  published on {new Date(Number(data.post.createdAt)).toLocaleDateString()}
+                  published on{" "}
+                  {new Date(Number(data.post.createdAt)).toLocaleDateString()}
                 </div>
               </div>
             </div>
@@ -69,7 +72,42 @@ const ParagraphXyzEmbed: React.FC<ParagraphXyzEmbedProps> = ({ url }) => {
     </Card>
   );
 
-  const renderPublication = () => <div></div>;
+  const renderPublication = () => (
+    <Card>
+      <CardHeader>
+        <CardTitle>{data.post.title} on Paragraph</CardTitle>
+        <CardDescription>
+          <div className="flex justify-between space-x-3">
+            <div className="flex flex-1 space-x-1 justify-between">
+              <div className="flex-shrink-0 justify-center flex flex-col">
+                <Avatar>
+                  <AvatarImage src={data.user.avatar_url} />
+                  <AvatarFallback>
+                    {data.user.authorName || data.publication.name}
+                  </AvatarFallback>
+                </Avatar>
+              </div>
+              <div className="ml-2 flex-1 min-w-0 flex flex-col justify-center -space-y-0.5">
+                <div className="flex flex-row space-x-2">
+                  {data.user.authorName}
+                </div>
+                <div className="flex flex-row space-x-2">
+                  published on{" "}
+                  {new Date(Number(data.post.createdAt)).toLocaleDateString()}
+                </div>
+              </div>
+            </div>
+          </div>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p className="max-w-lg">{data.post.post_preview}</p>
+      </CardContent>
+      <CardFooter onClick={() => openWindow(url)}>
+        <p>Read more on Pargraph</p>
+      </CardFooter>
+    </Card>
+  );
 
   const renderData = () => {
     if (data.post) {
