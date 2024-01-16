df858e08566bb45d9954fbaf89f87e05bb23df3c
hellno <hellno@users.noreply.github.com>
2024-01-16 13:35:47 +0100

    update wagmi, rainbowkit use

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,26 +5,28 @@ import { ThemeProvider } from "../src/common/hooks/ThemeProvider";
 import { WagmiConfig } from "wagmi";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import CommandPalette from "../src/common/components/CommandPalette";
-import {
-  wagmiConfig,
-  chains,
-  rainbowKitTheme,
-} from "../src/common/helpers/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { rainbowKitTheme, config } from "../src/common/helpers/rainbowkit";
 import Home from "../src/home";
 
 import "../src/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 
+const queryClient = new QueryClient();
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   const children = (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains} theme={rainbowKitTheme}>
-        <CommandPalette />
-        <Home>
-          <Component {...pageProps} />
-        </Home>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider theme={rainbowKitTheme}>
+          <CommandPalette />
+          <Home>
+            <Component {...pageProps} />
+          </Home>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 
   const content = process.env.NEXT_PUBLIC_APTABASE_KEY ? (
--- src/common/helpers/rainbowkit.tsx
+++ src/common/helpers/rainbowkit.tsx
@@ -1,35 +1,31 @@
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-    getDefaultWallets,
-    midnightTheme,
-} from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig } from 'wagmi';
-import {
-    optimism,
-} from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, getDefaultWallets, midnightTheme } from "@rainbow-me/rainbowkit";
+import { optimism } from "@wagmi/core/chains";
+import { http, createConfig } from "@wagmi/core";
 
-export const { chains, publicClient } = configureChains(
-    [optimism],
-    [
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! }),
-    ]
-);
-
-const { connectors } = getDefaultWallets({
-    appName: 'herocast',
-    projectId: 'b34f1019e33e832831871e41741f13fc',
-    chains
+export const { chains } = createConfig({
+  chains: [optimism],
+  transports: {
+    [optimism.id]: http(
+      `https://opt-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
+    ),
+  },
 });
 
-export const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors,
-    publicClient
-})
+export const config = getDefaultConfig({
+  appName: "herocast",
+  projectId: "b34f1019e33e832831871e41741f13fc",
+  chains: [optimism],
+});
+
+// export const wagmiConfig = createConfig({
+//   autoConnect: true,
+//   connectors,
+//   publicClient,
+// });
 
 export const rainbowKitTheme = midnightTheme({
-    accentColorForeground: 'white',
-    borderRadius: 'small',
-    fontStack: 'system',
+  accentColorForeground: "white",
+  borderRadius: "small",
+  fontStack: "system",
 });
