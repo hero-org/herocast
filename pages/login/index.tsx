132ccc7360f306b5bb61b042e3a0c105a8f30105
hellno <hellno@users.noreply.github.com>
2024-02-10 15:00:39 +0100

    Fix login on mobile

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -114,8 +114,8 @@ export default function Login() {
           className="hidden dark:block"
         />
       </div>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+      <div className="container relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative h-full flex-col bg-muted p-10 text-white flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             herocast
