e13da20f1fce78d516d5ee9ee45aa453ee1cd3d5
hellno <hellno@users.noreply.github.com>
2024-05-01 17:12:23 -0700

    fix build

--- api/frames/route.tsx
+++ api/frames/route.tsx
@@ -1,1 +1,7 @@
 export { GET, POST } from "@frames.js/render/next";
+
+export const config = {
+  unstable_allowDynamic: [
+    "/node_modules/@protobufjs/**", // use a glob to allow anything in the function-bind 3rd party module
+  ],
+};
