5714231d2bcaab2dd8e32bd232403cb05fa0d922
Benjamin Shafii <ben@prologe.io>
2024-05-25 10:41:14 +0200

    feat: ability to use query param in new feed

---
+++ pages/new-feed/index.tsx
@@ -0,0 +1,353 @@
+import React, { useEffect, useState } from "react";
+import {
+  AccountObjectType,
+  CUSTOM_CHANNELS,
+  useAccountStore,
+} from "@/stores/useAccountStore";
+import { useHotkeys } from "react-hotkeys-hook";
+import get from "lodash.get";
+import { CastRow } from "@/common/components/CastRow";
+import isEmpty from "lodash.isempty";
+import { CastThreadView } from "@/common/components/CastThreadView";
+import { SelectableListWithHotkeys } from "@/common/components/SelectableListWithHotkeys";
+import { Key } from "ts-key-enum";
+import ReplyModal from "@/common/components/ReplyModal";
+import EmbedsModal from "@/common/components/EmbedsModal";
+import { useInView } from "react-intersection-observer";
+import { Button } from "@/components/ui/button";
+import { FilterType, NeynarAPIClient } from "@neynar/nodejs-sdk";
+import {
+  CastWithInteractions,
+  FeedType,
+} from "@neynar/nodejs-sdk/build/neynar-api/v2";
+import { Loading } from "@/common/components/Loading";
+import uniqBy from "lodash.uniqby";
+import WelcomeCards from "@/common/components/WelcomeCards";
+import { useDataStore } from "@/stores/useDataStore";
+type FeedsType = {
+  [key: string]: CastWithInteractions[];
+};
+
+const DEFAULT_FEED_PAGE_SIZE = 10;
+const neynarClient = new NeynarAPIClient(
+  process.env.NEXT_PUBLIC_NEYNAR_API_KEY!
+);
+
+export default function Feed() {
+  const [feeds, setFeeds] = useState<FeedsType>({});
+  const [isLoadingFeed, setIsLoadingFeed] = useState(false);
+  const [nextFeedCursor, setNextFeedCursor] = useState("");
+  const [selectedCastIdx, setSelectedCastIdx] = useState(0);
+  const [showCastThreadView, setShowCastThreadView] = useState(false);
+  const [showReplyModal, setShowReplyModal] = useState(false);
+  const [showEmbedsModal, setShowEmbedsModal] = useState(false);
+  const [userFid, setUserFid] = useState("11115"); // Default fid
+
+  const { ref: buttonRef, inView } = useInView({
+    threshold: 0,
+    delay: 100,
+  });
+
+  const { accounts, selectedAccountIdx, selectedChannelUrl, hydratedAt } =
+    useAccountStore();
+
+  const { selectedCast, updateSelectedCast } = useDataStore();
+
+  const account: AccountObjectType = accounts[selectedAccountIdx];
+
+  const getFeedKey = ({
+    selectedChannelUrl,
+    account,
+  }: {
+    selectedChannelUrl: string | null;
+    account: AccountObjectType;
+  }) => {
+    if (selectedChannelUrl) {
+      return selectedChannelUrl;
+    } else if (account) {
+      return userFid;
+    } else {
+      return null;
+    }
+  };
+
+  const feedKey = getFeedKey({ selectedChannelUrl, account });
+  const feed = feedKey ? get(feeds, feedKey, []) : [];
+
+  const onOpenLinkInCast = (idx: number) => {
+    setShowEmbedsModal(true);
+  };
+
+  const onSelectCast = (idx: number) => {
+    setSelectedCastIdx(idx);
+    setShowCastThreadView(true);
+  };
+
+  useEffect(() => {
+    if (!showCastThreadView) {
+      if (selectedCastIdx === 0) {
+        window.scrollTo(0, 0);
+      } else if (selectedCastIdx === feed.length - 1) {
+        window.scrollTo(0, document.body.scrollHeight);
+      }
+    }
+  }, [selectedCastIdx, showCastThreadView]);
+
+  useEffect(() => {
+    updateSelectedCast(feed[selectedCastIdx]);
+  }, [selectedCastIdx, selectedChannelUrl, feed]);
+
+  useEffect(() => {
+    if (
+      isLoadingFeed ||
+      isEmpty(feed) ||
+      showCastThreadView ||
+      feed.length < DEFAULT_FEED_PAGE_SIZE ||
+      !inView
+    )
+      return;
+
+    getFeed({
+      fid: userFid,
+      parentUrl: selectedChannelUrl,
+      cursor: nextFeedCursor,
+    });
+  }, [
+    selectedCastIdx,
+    feed,
+    account,
+    selectedChannelUrl,
+    inView,
+    isLoadingFeed,
+  ]);
+
+  useHotkeys(
+    [Key.Escape, "§"],
+    () => {
+      setShowCastThreadView(false);
+    },
+    [showCastThreadView, showReplyModal, showEmbedsModal],
+    {
+      enableOnFormTags: true,
+      enableOnContentEditable: true,
+      enabled: showCastThreadView && !showReplyModal && !showEmbedsModal,
+    }
+  );
+
+  useHotkeys(
+    "r",
+    () => {
+      setShowReplyModal(true);
+    },
+    [showReplyModal],
+    {
+      enabled: !showReplyModal,
+      enableOnFormTags: false,
+      preventDefault: true,
+    }
+  );
+
+  const getFeedType = (parentUrl: string | undefined) =>
+    parentUrl === CUSTOM_CHANNELS.FOLLOWING
+      ? FeedType.Following
+      : FeedType.Filter;
+
+  const getFilterType = (parentUrl: string | undefined) => {
+    if (parentUrl === CUSTOM_CHANNELS.FOLLOWING) return undefined;
+    if (parentUrl === CUSTOM_CHANNELS.TRENDING)
+      return FilterType.GlobalTrending;
+    return FilterType.ParentUrl;
+  };
+
+  const getParentUrl = (parentUrl: string | undefined) =>
+    parentUrl === CUSTOM_CHANNELS.FOLLOWING ||
+    parentUrl === CUSTOM_CHANNELS.TRENDING
+      ? undefined
+      : parentUrl;
+
+  const getFeed = async ({
+    fid,
+    parentUrl,
+    cursor,
+  }: {
+    fid: string;
+    parentUrl?: string;
+    cursor?: string;
+  }) => {
+    if (isLoadingFeed) {
+      return;
+    }
+    setIsLoadingFeed(true);
+
+    try {
+      let feedOptions = {
+        cursor,
+        limit: DEFAULT_FEED_PAGE_SIZE,
+      };
+
+      let newFeed;
+      if (parentUrl === CUSTOM_CHANNELS.FOLLOWING) {
+        newFeed = await neynarClient.fetchUserFollowingFeed(
+          Number(fid),
+          feedOptions
+        );
+      } else {
+        feedOptions = {
+          ...feedOptions,
+          filterType: getFilterType(parentUrl),
+          parentUrl: getParentUrl(parentUrl),
+          fid: Number(fid),
+        } as {
+          cursor: string | undefined;
+          limit: number;
+          filterType: FilterType;
+          parentUrl: string;
+          fid: number;
+        };
+        newFeed = await neynarClient.fetchFeed(
+          getFeedType(parentUrl),
+          feedOptions
+        );
+      }
+      const feedKey = parentUrl || fid;
+      const feed = feeds[feedKey] || [];
+
+      setFeeds({
+        ...feeds,
+        [feedKey]: uniqBy(feed.concat(newFeed.casts), "hash"),
+      });
+      if (newFeed?.next?.cursor) {
+        setNextFeedCursor(newFeed.next.cursor);
+      }
+    } catch (e) {
+      console.error("Error fetching feed", e);
+    } finally {
+      setIsLoadingFeed(false);
+    }
+  };
+
+  useEffect(() => {
+    getFeed({ parentUrl: selectedChannelUrl, fid: userFid });
+  }, [account, selectedChannelUrl, showCastThreadView, userFid]);
+
+  useEffect(() => {
+    setShowReplyModal(false);
+    setShowCastThreadView(false);
+    setSelectedCastIdx(0);
+  }, [selectedChannelUrl]);
+
+  const renderRow = (item: any, idx: number) => (
+    <li
+      key={item?.hash}
+      className="border-b border-gray-700/40 relative flex items-center space-x-4 max-w-full"
+    >
+      <CastRow
+        cast={item}
+        isSelected={selectedCastIdx === idx}
+        onSelect={() => onSelectCast(idx)}
+        onReply={() => {
+          updateSelectedCast(item);
+          setShowReplyModal(true);
+        }}
+        showChannel
+      />
+    </li>
+  );
+
+  const getButtonText = (): string => {
+    if (isLoadingFeed) {
+      return "Loading...";
+    } else if (feed.length === 0) {
+      return "Load feed";
+    } else {
+      return "Load more";
+    }
+  };
+
+  const renderLoadMoreButton = () => (
+    <Button
+      ref={buttonRef}
+      onClick={() =>
+        getFeed({
+          fid: userFid,
+          parentUrl: selectedChannelUrl,
+          cursor: nextFeedCursor,
+        })
+      }
+      variant="outline"
+      className="ml-4 my-4 "
+    >
+      {getButtonText()}
+    </Button>
+  );
+
+  const renderFeed = () => (
+    <SelectableListWithHotkeys
+      data={feed}
+      selectedIdx={selectedCastIdx}
+      setSelectedIdx={setSelectedCastIdx}
+      renderRow={(item: any, idx: number) => renderRow(item, idx)}
+      onExpand={onOpenLinkInCast}
+      onSelect={onSelectCast}
+      isActive={!(showCastThreadView || showReplyModal || showEmbedsModal)}
+    />
+  );
+
+  const renderThread = () => (
+    <CastThreadView
+      cast={feed[selectedCastIdx]}
+      fid={userFid}
+      onBack={() => setShowCastThreadView(false)}
+      setSelectedCast={updateSelectedCast}
+      setShowReplyModal={setShowReplyModal}
+    />
+  );
+
+  const renderReplyModal = () => (
+    <ReplyModal
+      open={showReplyModal}
+      setOpen={() => setShowReplyModal(false)}
+      parentCast={selectedCast}
+    />
+  );
+
+  const renderEmbedsModal = () => {
+    return (
+      <EmbedsModal
+        open={showEmbedsModal}
+        setOpen={() => setShowEmbedsModal(false)}
+        cast={selectedCast}
+      />
+    );
+  };
+
+  const renderWelcomeMessage = () =>
+    feed.length === 0 && hydratedAt && !isLoadingFeed && <WelcomeCards />;
+
+  const renderContent = () => (
+    <>
+      <div className="min-w-full">
+        {isLoadingFeed && isEmpty(feed) && (
+          <div className="ml-4">
+            <Loading />
+          </div>
+        )}
+        {showCastThreadView ? (
+          renderThread()
+        ) : (
+          <>
+            {renderFeed()}
+            {renderWelcomeMessage()}
+            {feed.length > 0 &&
+              feed.length >= DEFAULT_FEED_PAGE_SIZE &&
+              renderLoadMoreButton()}
+          </>
+        )}
+      </div>
+      {renderReplyModal()}
+      {renderEmbedsModal()}
+    </>
+  );
+
+  return renderContent();
+}
