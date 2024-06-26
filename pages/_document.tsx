48b6a3df8a2fe621ad34478437fbcee4e5cdfd28
hellno <hellno@users.noreply.github.com>
2024-06-26 19:18:04 +0200

    fix open-graph tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,8 +11,11 @@ export default function Document() {
         <meta property="og:site_name" content="herocast" />
         <meta property="og:title" content="herocast" />
         <meta property="og:type" content="website" />
-        <meta property="og:description" content="herocast - Build, engage and grow on Farcaster. Faster." />
+        <meta property="og:description" content="herocast - #1 open-source Farcaster client for professionals and teams" />
         <meta property="og:url" content="https://app.herocast.xyz" />
+        <meta property="og:image" content="/images/herocast_og.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="twitter:card" content="summary_large_image" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
