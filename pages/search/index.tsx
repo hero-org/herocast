89d3eb584a8a741ac325182d90121ac524706081
hellno <hellno@users.noreply.github.com>
2024-07-03 17:39:14 +0200

    wip

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { SelectableListWithHotkeys } from "@/common/components/SelectableListWithHotkeys";
 import { CastRow } from "@/common/components/CastRow";
@@ -20,44 +21,55 @@ import { useDataStore } from "@/stores/useDataStore";
 import isEmpty from "lodash.isempty";
 import { useListStore } from "@/stores/useListStore";
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
-import { uniq } from "lodash";
+import { map, uniq } from "lodash";
 import SkeletonCastRow from "@/common/components/SkeletonCastRow";
+import { Switch } from "@/components/ui/switch";
 
-const getSearchUrl = (
-  searchTerm: string,
-  limit?: number,
-  offset?: number,
-  interval?: string,
-  orderBy?: string
-): string => {
-  const params = new URLSearchParams({ term: searchTerm });
-  if (limit) params.append("limit", limit.toString());
-  if (offset) params.append("offset", offset.toString());
-  if (interval) params.append("interval", interval);
-  if (orderBy) params.append("orderBy", orderBy);
-  const url = `/api/search?${params.toString()}`;
-  console.log("getSearchUrl", url);
-  return url;
+type SearchFilters = {
+  filterByPowerBadge: boolean;
 };
 
 type SearchForTextParams = {
   searchTerm: string;
+  filters?: SearchFilters;
   limit?: number;
   offset?: number;
   interval?: string;
   orderBy?: string;
 };
-
-const searchForText = async ({
+const getSearchUrl = ({
   searchTerm,
+  filters,
   limit,
   offset,
   interval,
   orderBy,
-}: SearchForTextParams) => {
+}: SearchForTextParams): string => {
+  const params = new URLSearchParams({ term: searchTerm });
+  if (limit) params.append("limit", limit.toString());
+  if (offset) params.append("offset", offset.toString());
+  if (interval) params.append("interval", interval);
+  if (orderBy) params.append("orderBy", orderBy);
+  if (filters) {
+    Object.keys(filters).forEach((key) => {
+      params.append(key, filters[key].toString());
+    });
+  }
+  const url = `/api/search?${params.toString()}`;
+  return url;
+};
+
+const searchForText = async ({
+  searchTerm,
+  filters,
+  limit,
+  offset,
+  interval,
+  orderBy,
+}: SearchForTextParams): Promise<RawSearchResult[]> => {
   try {
     const response = await fetch(
-      getSearchUrl(searchTerm, limit, offset, interval, orderBy)
+      getSearchUrl({ searchTerm, filters, limit, offset, interval, orderBy })
     );
     const data = await response.json();
     if (!data || data?.error) return [];
@@ -69,17 +81,29 @@ const searchForText = async ({
 };
 
 const APP_FID = process.env.NEXT_PUBLIC_APP_FID!;
-const SEARCH_LIMIT_INITIAL_LOAD = 6;
+const SEARCH_LIMIT_INITIAL_LOAD = 2;
 const SEARCH_LIMIT_NEXT_LOAD = 10;
 const SEARCH_LIMIT = SEARCH_LIMIT_INITIAL_LOAD + SEARCH_LIMIT_NEXT_LOAD - 1;
+const DEFAULT_FILTERS: SearchFilters = {
+  filterByPowerBadge: true,
+};
+
+type RawSearchResult = {
+  hash: string;
+  fid: number;
+  text: string;
+  timestamp: string;
+};
 
 export default function SearchPage() {
   const [searchTerm, setSearchTerm] = useState("");
   const [casts, setCasts] = useState<CastWithInteractions[]>([]);
-  const [castHashes, setCastHashes] = useState<string[]>([]);
+  const [castHashes, setCastHashes] = useState<RawSearchResult[]>([]);
   const [selectedCastIdx, setSelectedCastIdx] = useState(-1);
   const [isLoading, setIsLoading] = useState(false);
   const [searchCounter, setSearchCounter] = useState(0);
+  const [filterByPowerBadge, setFilterByPowerBadge] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
   const activeSearchCounter = useRef(0);
 
   const {
@@ -102,7 +126,7 @@ export default function SearchPage() {
     const searchParam = urlParams.get("search");
     if (searchParam) {
       setSearchTerm(searchParam);
-      onSearch(searchParam);
+      onSearch(searchParam, true);
     }
 
     const listId = urlParams.get("list");
@@ -129,10 +153,16 @@ export default function SearchPage() {
 
   useEffect(() => {
     if (selectedList && selectedList.type === "search") {
-      const { term } = selectedList.contents as { term?: string };
+      const { term, filters } = selectedList.contents as {
+        term?: string;
+        filters?: any;
+      };
+      const { filterByPowerBadge } = filters || DEFAULT_FILTERS;
+      setFilterByPowerBadge(filterByPowerBadge);
+
       if (term) {
         setSearchTerm(term);
-        onSearch(term);
+        onSearch(term, filterByPowerBadge);
       }
     }
   }, [selectedList]);
@@ -141,12 +171,17 @@ export default function SearchPage() {
     setSearchTerm(text);
   };
 
-  const addCastHashes = (newCastHashes: string[]) => {
-    setCastHashes(uniq([...castHashes, ...newCastHashes]));
+  const addCastHashes = (newCastHashes: RawSearchResult[], reset: boolean) => {
+    setCastHashes((prevCastHashes) =>
+      uniq([...(reset ? [] : prevCastHashes), ...newCastHashes])
+    );
   };
 
+  const getFilters = () =>
+    filterByPowerBadge ? { filterByPowerBadge } : undefined;
+
   const onSearch = useCallback(
-    async (term?: string) => {
+    async (term?: string, filterByPowerBadge?: boolean) => {
       const newSearchCounter = searchCounter + 1;
       setSearchCounter(newSearchCounter);
       activeSearchCounter.current = newSearchCounter;
@@ -155,6 +190,7 @@ export default function SearchPage() {
         term = searchTerm;
       }
 
+      setError(null);
       setIsLoading(true);
       setCasts([]);
       setCastHashes([]);
@@ -162,24 +198,30 @@ export default function SearchPage() {
       const startedAt = Date.now();
 
       try {
+        const filters = getFilters();
         const searchResults = await searchForText({
           searchTerm: term,
+          filters,
+          orderBy: "timestamp DESC",
           limit: SEARCH_LIMIT_INITIAL_LOAD,
         });
         if (activeSearchCounter.current !== newSearchCounter) {
           return;
         }
+
         if (searchResults.length > 0) {
-          addCastHashes(searchResults.map((cast) => cast.hash));
-          const endedAt = Date.now();
+          addCastHashes(searchResults, true);
+          const endedAt1 = Date.now();
           addSearch({
-            term,
+            term: `${term}-${newSearchCounter}`,
             startedAt,
-            endedAt,
+            endedAt: endedAt1,
             resultsCount: searchResults.length,
           });
           const moreResults = await searchForText({
             searchTerm: term,
+            filters,
+            offset: SEARCH_LIMIT_INITIAL_LOAD,
             limit: SEARCH_LIMIT_NEXT_LOAD,
             orderBy: "timestamp DESC",
           });
@@ -187,8 +229,15 @@ export default function SearchPage() {
             return;
           }
           if (moreResults.length > 0) {
-            addCastHashes(moreResults.map((cast) => cast.hash));
+            addCastHashes(moreResults, false);
           }
+          const endedAt2 = Date.now();
+          addSearch({
+            term: `${term}-${newSearchCounter}-more`,
+            startedAt: endedAt1,
+            endedAt: endedAt2,
+            resultsCount: moreResults.length,
+          });
         }
       } catch (error) {
         console.error("Failed to search for text", term, error);
@@ -196,7 +245,7 @@ export default function SearchPage() {
         setIsLoading(false);
       }
     },
-    [searchCounter, searchTerm]
+    [searchCounter, searchTerm, filterByPowerBadge]
   );
 
   const onSaveSearch = async () => {
@@ -232,15 +281,16 @@ export default function SearchPage() {
         );
         setCasts(sortedCasts);
       } catch (error) {
+        setError(error);
         console.error("Failed to fetch casts", newCastHashes, error);
       }
     };
 
-    const newCastHashes = castHashes.filter(
+    const newCastHashes = map(castHashes, "hash").filter(
       (hash) => !casts.find((cast) => cast.hash === hash)
     );
     if (newCastHashes.length > 0) {
-      fetchCasts(newCastHashes.slice(0, 5));
+      // fetchCasts(newCastHashes);
     }
   }, [castHashes, casts]);
 
@@ -254,15 +304,18 @@ export default function SearchPage() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {searches.map((metric) => (
-          <TableRow key={metric.startedAt}>
-            <TableCell className="font-medium">
-              {metric.endedAt - metric.startedAt}
-            </TableCell>
-            <TableCell>{metric.resultsCount}</TableCell>
-            <TableCell>{metric.term}</TableCell>
-          </TableRow>
-        ))}
+        {searches
+          .toReversed()
+          .slice(0, 10)
+          .map((metric) => (
+            <TableRow key={metric.startedAt}>
+              <TableCell className="font-medium">
+                {(metric.endedAt - metric.startedAt) / 1000}s
+              </TableCell>
+              <TableCell>{metric.resultsCount}</TableCell>
+              <TableCell>{metric.term}</TableCell>
+            </TableRow>
+          ))}
       </TableBody>
     </Table>
   );
@@ -282,25 +335,25 @@ export default function SearchPage() {
   );
 
   const renderLoadMoreButton = () => (
-    <div className="flex justify-center my-8">
-      <Button
-        size="lg"
-        disabled={isLoading}
-        onClick={() => {
-          setIsLoading(true);
-          searchForText({
-            searchTerm,
-            limit: SEARCH_LIMIT_NEXT_LOAD,
-            offset: castHashes.length,
-          }).then((results) => {
-            setCastHashes([...castHashes, ...results.map((cast) => cast.hash)]);
-            setIsLoading(false);
-          });
-        }}
-      >
-        Load More
-      </Button>
-    </div>
+    <Button
+      size="lg"
+      disabled={isLoading}
+      onClick={() => {
+        setIsLoading(true);
+        searchForText({
+          searchTerm,
+          filters: getFilters(),
+          limit: SEARCH_LIMIT_NEXT_LOAD,
+          orderBy: "timestamp DESC",
+          offset: castHashes.length,
+        }).then((results) => {
+          addCastHashes(results, false);
+          setIsLoading(false);
+        });
+      }}
+    >
+      Load More
+    </Button>
   );
 
   const renderLoadingSpinner = () => (
@@ -321,10 +374,33 @@ export default function SearchPage() {
           <SkeletonCastRow />
         </>
       ) : (
-        castHashes.map((hash) => <SkeletonCastRow key={`skeleton-${hash}`} />)
+        castHashes.map((obj) => (
+          <SkeletonCastRow key={`skeleton-${obj?.hash}`} text={obj.text} />
+        ))
       )}
     </div>
   );
+
+  const renderPowerBadgeFilter = () => (
+    <Button
+      variant={filterByPowerBadge ? "outline" : "ghost"}
+      size="sm"
+      onClick={() => setFilterByPowerBadge((prev) => !prev)}
+    >
+      Only Power Badge
+      <img
+        src="/images/ActiveBadge.webp"
+        className="ml-1 h-4 w-4"
+        alt="power badge"
+      />
+      <Switch
+        className="ml-1"
+        aria-label="Toggle powerbadge"
+        checked={filterByPowerBadge}
+      />
+    </Button>
+  );
+
   return (
     <div className="min-w-0 flex-1 p-6">
       <div className="w-full max-w-2xl">
@@ -361,11 +437,20 @@ export default function SearchPage() {
             Save
           </Button>
         </div>
+        <div className="flex w-full max-w-lg mt-4">
+          {renderPowerBadgeFilter()}
+        </div>
       </div>
-      {isLoading && casts.length === 0 && renderLoading()}
+      {isLoading ||
+        (castHashes.length !== 0 && casts.length === 0 && renderLoading())}
       {!isLoading && searches.length > 0 && castHashes.length === 0 && (
         <div className="text-center mt-8 text-muted-foreground">
           No results found
+        </div>
+      )}
+      {error && (
+        <div className="text-center mt-8 text-red-500">
+          Error: {error.message}
         </div>
       )}
       <SelectableListWithHotkeys
@@ -375,8 +460,11 @@ export default function SearchPage() {
         setSelectedIdx={setSelectedCastIdx}
         onSelect={(idx) => setSelectedCastIdx(idx)}
       />
-      {casts.length >= SEARCH_LIMIT &&
-        (isLoading ? renderLoadingSpinner() : renderLoadMoreButton())}
+      {casts.length >= SEARCH_LIMIT && (
+        <div className="flex justify-center my-8">
+          {isLoading ? renderLoadingSpinner() : renderLoadMoreButton()}
+        </div>
+      )}
     </div>
   );
 }
--- src/common/components/SkeletonCastRow.tsx
+++ src/common/components/SkeletonCastRow.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 
-const SkeletonCastRow = () => {
+type SkeletonCastRowProps = {
+  text?: string;
+};
+const SkeletonCastRow = ({ text }: SkeletonCastRowProps) => {
   const randomDelay = Math.floor(Math.random() * 2000);
   return (
     <div className="flex items-start mx-2 space-x-4">
@@ -9,20 +12,26 @@ const SkeletonCastRow = () => {
         className="h-10 w-10 rounded-full"
         style={{ animationDelay: `${randomDelay + 100}ms` }}
       />
-      <div className="flex-1 space-y-4">
+      <div className="flex-1 space-y-2">
         <Skeleton
           className="h-4 w-1/4 rounded"
           style={{ animationDelay: `${randomDelay + 100}ms` }}
         />
         <div className="flex-1 space-y-2">
-          <Skeleton
-            className="h-4 w-3/4 rounded"
-            style={{ animationDelay: `${randomDelay + 300}ms` }}
-          />
-          <Skeleton
-            className="h-4 w-1/2 rounded"
-            style={{ animationDelay: `${randomDelay + 300}ms` }}
-          />
+          {text ? (
+            <div className="flex space-x-2">{text}</div>
+          ) : (
+            <>
+              <Skeleton
+                className="h-4 w-3/4 rounded"
+                style={{ animationDelay: `${randomDelay + 300}ms` }}
+              />
+              <Skeleton
+                className="h-4 w-1/2 rounded"
+                style={{ animationDelay: `${randomDelay + 300}ms` }}
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
