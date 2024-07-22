457723c2f267abc809c0e5f23fbce32dd726b4d5
hellno <hellno@users.noreply.github.com>
2024-07-22 21:17:56 +0200

    self CR

--- pages/api/additionalProfileInfo.ts
+++ pages/api/additionalProfileInfo.ts
@@ -52,7 +52,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       getSocialCapitalScore(fid),
       getIcebreakerData(fid)
     ]);
-    console.log(`icebreakerData for fid ${fid}`, icebreakerData);
     res.status(200).json({ socialCapitalScore, icebreakerData });
   } catch (error) {
     console.error('Error fetching additional profile info:', error);
--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -1,36 +1,9 @@
-"use client"
-
-import * as React from "react"
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
 
 const Collapsible = CollapsiblePrimitive.Root
 
-const CollapsibleTrigger = React.forwardRef<
-  React.ElementRef<typeof CollapsiblePrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <CollapsiblePrimitive.Trigger
-    ref={ref}
-    className={className}
-    {...props}
-  >
-    {children}
-  </CollapsiblePrimitive.Trigger>
-))
-CollapsibleTrigger.displayName = CollapsiblePrimitive.Trigger.displayName
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
-const CollapsibleContent = React.forwardRef<
-  React.ElementRef<typeof CollapsiblePrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Content>
->(({ className, children, ...props }, ref) => (
-  <CollapsiblePrimitive.Content
-    ref={ref}
-    className={className}
-    {...props}
-  >
-    {children}
-  </CollapsiblePrimitive.Content>
-))
-CollapsibleContent.displayName = CollapsiblePrimitive.Content.displayName
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
