eb2d2bd2cd18dd15962c2e7585e22a2c9cd5dce2
hellno <hellno@users.noreply.github.com>
2024-08-06 16:25:50 +0200

    feat: Enable row-level security and grant permissions for analytics table

--- supabase/migrations/20240806141703_analytics.sql
+++ supabase/migrations/20240806141703_analytics.sql
@@ -6,31 +6,12 @@ CREATE TABLE IF NOT EXISTS "public"."analytics" (
 );
 
 -- Enable Row Level Security
-ALTER TABLE "public"."analytics" ENABLE ROW LEVEL SECURITY;
-
--- Create a policy that allows everyone to select from the table
-CREATE POLICY "Allow select for everyone" ON "public"."analytics"
-    FOR SELECT
-    USING (true);
-
--- Create policies for insert, update, and delete (optional, adjust as needed)
--- For example, you might want to restrict these operations to authenticated users or specific roles
-CREATE POLICY "Allow insert for authenticated users" ON "public"."analytics"
-    FOR INSERT
-    TO authenticated
-    WITH CHECK (true);
-
-CREATE POLICY "Allow update for authenticated users" ON "public"."analytics"
-    FOR UPDATE
-    TO authenticated
-    USING (true)
-    WITH CHECK (true);
-
-CREATE POLICY "Allow delete for authenticated users" ON "public"."analytics"
-    FOR DELETE
-    TO authenticated
-    USING (true);
+ALTER TABLE
+    "public"."analytics" ENABLE ROW LEVEL SECURITY;
 
 -- Grant necessary privileges
 GRANT ALL ON "public"."analytics" TO authenticated;
-GRANT SELECT ON "public"."analytics" TO anon;
+
+GRANT
+SELECT
+    ON "public"."analytics" TO anon;
