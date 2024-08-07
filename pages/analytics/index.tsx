e3b4f11045331c20bb0c6952f9165f864d4df364
hellno <hellno@users.noreply.github.com>
2024-08-07 16:49:20 +0200

    feat: Add CastReactionsTable component to Analytics page

--- pages/analytics/index.tsx
+++ pages/analytics/index.tsx
@@ -1,48 +1,59 @@
 import NewFollowersCard from "@/common/components/Analytics/NewFollowersCard";
 import ReactionsCard from "@/common/components/Analytics/ReactionsCard";
+import { createClient } from "@/common/helpers/supabase/component";
+import { AnalyticsData } from "@/common/types/types";
 import { useAccountStore } from "@/stores/useAccountStore";
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import React, { useEffect, useState } from "react";
 
 export default function AnalyticsPage() {
-  const supabase = useSupabaseClient();
-  const [analyticsData, setAnalyticsData] = useState(null);
+  const supabaseClient = createClient();
+  const [analyticsData, setAnalyticsData] = useState<AnalyticsData>();
   const viewerFid = useAccountStore(
     (state) => state.accounts[state.selectedAccountIdx].platformAccountId
   );
-
   useEffect(() => {
     const fetchAnalytics = async () => {
       if (!viewerFid) return;
 
-      const { data, error } = await supabase
-        .from('analytics')
-        .select('data')
-        .eq('fid', viewerFid)
-        .single();
-
+      const { data: analyticsRow, error } = await supabaseClient
+        .from("analytics")
+        .select("*")
+        .eq("fid", viewerFid)
+        .maybeSingle();
       if (error) {
-        console.error('Error fetching analytics:', error);
-      } else {
-        setAnalyticsData(data?.data);
+        console.error("Error fetching analytics:", error);
+        return;
+      }
+      if (analyticsRow && analyticsRow.status === "done") {
+        const { fid, updated_at: updatedAt, data } = analyticsRow;
+        const analyticsData = {
+          fid,
+          updatedAt,
+          links: data?.links,
+          reactions: data?.reactions,
+        } as unknown as AnalyticsData;
+        setAnalyticsData(analyticsData);
       }
     };
 
     fetchAnalytics();
-  }, [viewerFid, supabase]);
+  }, [viewerFid]);
 
   if (!analyticsData) {
     return <div className="w-full m-8 text-center">Loading analytics...</div>;
   }
 
+  console.log("analyticsData", analyticsData);
   return (
     <div className="w-full m-8 grid grid-cols-1 md:grid-cols-2 gap-4">
-      {analyticsData.links && (
-        <NewFollowersCard resolution="daily" data={analyticsData.links} />
+      {analyticsData?.links && (
+        <NewFollowersCard data={analyticsData.links} />
       )}
-      {analyticsData.reactions && (
-        <ReactionsCard resolution="daily" data={analyticsData.reactions} />
+      {analyticsData?.reactions && (
+        <ReactionsCard data={analyticsData?.reactions} />
       )}
+      <div></div>
+      <CastReactionsTable data={analyticsData?.casts} />
     </div>
   );
 }
