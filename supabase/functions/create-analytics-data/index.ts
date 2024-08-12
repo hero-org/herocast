151f99c8f2a1069907eb4da523cf78b34d10d899
hellno <hellno@users.noreply.github.com>
2024-08-12 17:58:15 +0200

    fix: Update the endpoint name in the example curl request

--- supabase/functions/create-analytics-data/index.ts
+++ supabase/functions/create-analytics-data/index.ts
@@ -140,9 +140,9 @@ Deno.serve(async (req) => {
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/create_analytics_data' \
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/create-analytics-data' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
+    --data '{"name":"Functions", "fid": "3"}'
 
 */
