480e46f0243d015fbda803f9e9dc3f89df5fae07
hellno <hellno@users.noreply.github.com>
2024-08-21 17:11:02 +0200

    refactor: Improve date handling and add missing dates in analytics helper

--- src/common/helpers/analytics.ts
+++ src/common/helpers/analytics.ts
@@ -1,42 +1,48 @@
-import { subDays } from "date-fns";
+import { addDays, startOfDay, subDays } from "date-fns";
 import { AggregatedAnalytics } from "../types/types";
-import sortBy from "lodash.sortby";
+import { UTCDate } from "@date-fns/utc";
 
 export const fillMissingDaysBetweenDates = (data: AggregatedAnalytics[], startDate: Date, endDate: Date) => {
     console.log('data input', data)
-    const dataStartsAt = new Date(data[0].timestamp);
-    const dataEndsAt = new Date(data[data.length - 1].timestamp);
+    const dataStartsAt = new UTCDate(data[0].timestamp);
+    const dataEndsAt = new UTCDate(data[data.length - 1].timestamp);
+    const today = startOfDay(new UTCDate());
 
-    let missingDates = [];
+    const missingDates = [];
+    // if data starts after the start date, add missing dates before the data starts
     if (dataStartsAt > startDate) {
         for (let i = 0; i < dataStartsAt.getDate() - startDate.getDate(); i++) {
+            const timestamp = subDays(startDate, i).toISOString();
+            console.log('adding timestamp before', timestamp)
             missingDates.push({
-                timestamp: subDays(new Date(dataStartsAt.setHours(0, 0, 0, 0)), i).toISOString(),
+                timestamp,
                 count: 0,
             });
         }
     }
 
+    // if data ends before the end date, add missing dates after the data ends
     if (dataEndsAt < endDate) {
         for (let i = 0; i < endDate.getDate() - dataEndsAt.getDate(); i++) {
+            const timestamp = addDays(endDate, i).toISOString();
+            console.log('adding timestamp after ', timestamp)
             missingDates.push({
-                timestamp: new Date(startDate).toISOString(),
+                timestamp,
                 count: 0,
             });
         }
     }
 
     const missingDatesBetweenStartAndEnd = [];
-    // find missing dates between start and end
     for (let i = 0; i < data.length; i++) {
-        const currentDate = new Date(data[i].timestamp);
+        const currentDate = new UTCDate(data[i].timestamp);
         if (i + 1 < data.length) {
-            const nextDate = new Date(data[i + 1].timestamp);
+            const nextDate = new UTCDate(data[i + 1].timestamp);
             const diff = nextDate.getDate() - currentDate.getDate();
             if (diff > 1) {
                 for (let j = 1; j < diff; j++) {
                     missingDatesBetweenStartAndEnd.push({
-                        timestamp: subDays(new Date(currentDate), j).toISOString(),
+                        timestamp: subDays(new UTCDate(currentDate), j).toISOString(),
                         count: 0,
                     });
                 }
@@ -45,6 +51,6 @@ export const fillMissingDaysBetweenDates = (data: AggregatedAnalytics[], startDa
     }
 
     data = [...missingDates, ...data, ...missingDatesBetweenStartAndEnd];
-    console.log('data output', sortBy(data, "timestamp"))
-    return sortBy(data, "timestamp");
+    const sorted = data.sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime());
+    return sorted;
 };
