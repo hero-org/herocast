c6a4af3446bdc9c81e6103b2e71007631e11ac8d
hellno <hellno@users.noreply.github.com>
2024-08-26 18:42:27 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -49,15 +49,15 @@ const nextConfig = {
           },
         ],
       },
-      // {
-      //   source: '/api/additionalProfileInfo',
-      //   headers: [
-      //     {
-      //       key: 'Cache-Control',
-      //       value: 'public, max-age=43200, s-maxage=43200, stale-while-revalidate=43200',
-      //     },
-      //   ],
-      // },
+      {
+        source: '/api/additionalProfileInfo',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=43200, s-maxage=43200, stale-while-revalidate=43200',
+          },
+        ],
+      },
     ];
   },
 };
