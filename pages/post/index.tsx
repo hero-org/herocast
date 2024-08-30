d8310195288fe8b424a529497475144502549343
Standfirm <147502004+stanfy79@users.noreply.github.com>
2024-08-30 08:16:53 +0100

    Updated index.tsx

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -22,6 +22,7 @@ import { UUID } from 'crypto';
 import { usePathname, useSearchParams } from 'next/navigation';
 import UpgradeFreePlanCard from '@/common/components/UpgradeFreePlanCard';
 import { getPlanLimitsForPlan } from '@/config/planLimits';
+import newThread from './assets/plus-circle.png';  // Add newThread image import
 
 enum DraftListTab {
   writing = 'writing',
@@ -73,6 +74,10 @@ export default function NewPost() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const savedPathname = useRef(pathname);
+
+  // New State for Posts and Threads
+  const [posts, setPosts] = useState([{ id: 1, content: '', threads: [] }]);
+  const [allPosts, setAllPosts] = useState([]);
 
   const draftsForTab = useMemo(
     () => getDraftsForTab(drafts, activeTab, selectedAccount?.id),
@@ -128,6 +133,47 @@ export default function NewPost() {
     }
   }, [drafts]);
 
+  // Function to handle content change in the main post
+  const handleContentChange = (event, postId) => {
+    const newPosts = posts.map((post) => {
+      if (post.id === postId) {
+        return { ...post, content: event.target.value };
+      }
+      return post;
+    });
+    setPosts(newPosts);
+  };
+
+  // Function to add a new thread to a post
+  const addThread = (postId) => {
+    const newPosts = posts.map((post) => {
+      if (post.id === postId) {
+        return { ...post, threads: [...post.threads, ''] };
+      }
+      return post;
+    });
+    setPosts(newPosts);
+  };
+
+  // Function to handle content change in a thread
+  const handleThreadContentChange = (event, postId, index) => {
+    const newPosts = posts.map((post) => {
+      if (post.id === postId) {
+        const newThreads = [...post.threads];
+        newThreads[index] = event.target.value;
+        return { ...post, threads: newThreads };
+      }
+      return post;
+    });
+    setPosts(newPosts);
+  };
+
+  // Function to handle post submission
+  const handlePostSubmit = () => {
+    setAllPosts([...allPosts, ...posts]);
+    setPosts([{ id: posts.length + 1, content: '', threads: [] }]); // Reset posts
+  };
+
   const onRemove = (draft) => {
     removePostDraftById(draft.id);
   };
@@ -156,232 +202,74 @@ export default function NewPost() {
     );
   };
 
-  const renderScheduledDraft = (draft) => {
-    if (!draft) return renderEmptyMainContent();
-
-    const channel = getChannelForParentUrl({
-      channels: allChannels,
-      parentUrl: draft.parentUrl,
-    });
-
-    const parentCast = parentCasts.find((cast) => cast.hash === draft.parentCastId?.hash);
-    const hasEmbeds = draft?.embeds?.length > 0;
-    return (
-      <div className="pt-4 pb-6">
-        <div className="flex items-center text-xs text-muted-foreground">
-          <ClockIcon className="w-5 h-5" />
-          <span className="ml-1">
-            Scheduled for {getUserLocaleDateFromIsoString(draft.scheduledFor)}{' '}
-            {draft.publishedAt && `· Published at ${getUserLocaleDateFromIsoString(draft.publishedAt)}`}
-          </span>
-          {channel && (
-            <p>
-              &nbsp;in&nbsp;
-              <span className="h-5 inline-flex truncate items-top rounded-sm bg-blue-400/10 px-1.5 py-0.5 text-xs font-medium text-blue-400 ring-1 ring-inset ring-blue-400/30">
-                {channel.name}
-              </span>
-            </p>
-          )}
-        </div>
-        {parentCast && (
-          <div className="flex border p-2 mt-2">{<CastRow cast={parentCast} isEmbed hideReactions />}</div>
-        )}
-        <div className="mt-4 px-2 py-1 bg-muted border rounded-lg w-full h-full min-h-[60px] max-h-[150px]">
-          {draft.text}
-        </div>
-        {hasEmbeds && (
-          <div className="mt-8 rounded-md bg-muted p-2 w-full break-all">
-            {map(draft.embeds, (embed) => (
-              <div key={`cast-embed-${embed.url || embed.hash}`}>{renderEmbedForUrl(embed)}</div>
-            ))}
-          </div>
-        )}
-      </div>
-    );
-  };
-
-  const renderDraftListPreview = (draft) => {
-    const channel = getChannelForParentUrl({
-      channels: allChannels,
-      parentUrl: draft.parentUrl,
-    });
-    return (
-      <div
-        key={draft?.id || draft?.createdAt}
-        className={cn(
-          'flex flex-col max-w-full items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent cursor-pointer',
-          draft.id === selectedDraftId && 'bg-muted'
-        )}
-        onClick={() => {
-          setSelectedDraftId(draft.id);
-        }}
-      >
-        <div
-          className={cn(
-            'line-clamp-2 text-xs break-all',
-            draft.id === selectedDraftId ? 'text-foreground' : 'text-muted-foreground'
-          )}
-        >
-          {draft.text ? draft.text.substring(0, 300) : 'New cast'}
-        </div>
-        <div className="flex w-full flex-col gap-1">
-          <div className="flex items-center gap-2">
-            {draft?.embeds?.length ? (
-              <div className="flex items-center gap-2">
-                <Badge variant="outline">{localize(draft.embeds.length, ' embed')}</Badge>
-              </div>
-            ) : null}
-            {channel && (
-              <span className="h-5 inline-flex truncate items-top rounded-sm bg-blue-400/10 px-1.5 py-0.5 text-xs font-medium text-blue-400 ring-1 ring-inset ring-blue-400/30">
-                {channel.name}
-              </span>
-            )}
-            <div
-              className={cn(
-                'ml-auto text-xs',
-                draft.id === selectedDraftId ? 'text-foreground' : 'text-muted-foreground'
-              )}
-            >
-              {draft.status === DraftStatus.writing &&
-                draft.createdAt &&
-                formatDistanceToNow(new Date(draft.createdAt), {
-                  addSuffix: true,
-                })}
-            </div>
-            <Button
-              className="py-0.5 px-1 hover:bg-muted-foreground/20"
-              size="sm"
-              variant="outline"
-              onClick={() => onRemove(draft)}
-            >
-              <TrashIcon className="w-4 h-4" />
-            </Button>
-          </div>
-          <div className="text-xs font-medium">
-            {draft.scheduledFor && (
-              <div className="flex items-center gap-2">
-                <ClockIcon className="w-4 h-4" />
-                <span className="text-xs text-muted-foreground">
-                  Scheduled for {getUserLocaleDateFromIsoString(draft.scheduledFor, 'short', 'short')}
-                </span>
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    );
-  };
-
-  const renderTabsSelector = () => (
-    <div className="flex items-center justify-between">
-      <div className="flex items-center py-2 w-full">
-        <TabsList className="flex w-full">
-          {DraftListTabs.map((tab) => (
-            <TabsTrigger key={tab.key} value={tab.key} className="text-zinc-600 dark:text-zinc-200">
-              {tab.label}
-            </TabsTrigger>
-          ))}
-        </TabsList>
-      </div>
+  const renderNewDraftButton = () => (
+    <div className="flex justify-center mt-6">
+      <Button onClick={() => addNewPostDraft()} className="inline-flex items-center px-2 py-1">
+        <PencilSquareIcon className="w-4 h-4 text-muted-foreground inline mr-2" />
+        New Draft
+      </Button>
     </div>
   );
 
-  const renderScrollableList = (children: React.ReactElement) => (
-    <ScrollArea className="flex-1">
-      <div className="flex flex-col gap-2 pt-0">{children}</div>
-    </ScrollArea>
-  );
-
-  const renderNewDraftButton = () => (
-    <Button
-      size="sm"
-      variant="outline"
-      className="flex items-center gap-2 mx-auto"
-      onClick={() => {
-        setActiveTab(DraftListTab.writing);
-        addNewPostDraft({});
-      }}
-    >
-      <PencilSquareIcon className="w-5 h-5" />
-      <span>New draft</span>
-    </Button>
-  );
-
-  const renderDraftList = () => {
-    return renderScrollableList(
-      <>
-        <div className="flex flex-col gap-2 p-2 pt-0">{draftsForTab.map(renderDraftListPreview)}</div>
-        <div className="mt-4">{renderNewDraftButton()}</div>
-      </>
-    );
-  };
-
-  const renderScheduledList = () => {
-    return renderScrollableList(
-      <>
-        {draftsForTab.length === 0 && (
-          <Button
-            variant="outline"
-            className="flex items-center gap-2"
-            onClick={() => {
-              addNewPostDraft({});
-              setActiveTab(DraftListTab.writing);
-            }}
-          >
-            <PencilSquareIcon className="w-5 h-5" />
-            <span>New draft</span>
-          </Button>
-        )}
-        <div className="flex flex-col gap-2 p-2 pt-0">{draftsForTab.map(renderDraftListPreview)}</div>
-      </>
-    );
-  };
-
-  const renderContent = () => {
-    const draft = draftsForTab.find((draft) => draft.id === selectedDraftId);
-    switch (activeTab) {
-      case DraftListTab.writing:
-        return renderWritingDraft(draft);
-      case DraftListTab.scheduled:
-      case DraftListTab.published:
-        return renderScheduledDraft(draft);
-      default:
-        return null;
-    }
-  };
-
-  const renderFreePlanCard = () => {
-    const scheduledCastLimit = getPlanLimitsForPlan('openSource').maxScheduledCasts;
-    if (scheduledCastsCount < scheduledCastLimit) return null;
-
-    return <UpgradeFreePlanCard limitKey="maxScheduledCasts" />;
-  };
-
   return (
-    <div className="grid grid-cols-[300px_1fr] h-screen w-full">
-      <div className="overflow-y-auto p-4">
-        <div className="space-y-4">
-          <Tabs
-            defaultValue="drafts"
-            className="w-full"
-            value={activeTab}
-            onValueChange={(value) => setActiveTab(value as DraftListTab)}
-          >
-            {renderTabsSelector()}
-            <TabsContent value={DraftListTab.writing}>
-              {renderFreePlanCard()}
-              {renderDraftList()}
-            </TabsContent>
-            <TabsContent value={DraftListTab.scheduled}>
-              {renderFreePlanCard()}
-              {renderScheduledList()}
-            </TabsContent>
-            <TabsContent value={DraftListTab.published}>{renderScheduledList()}</TabsContent>
-          </Tabs>
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <div className="flex-1 overflow-y-auto p-4">{renderContent()}</div>
+    <div className="space-y-2 w-full">
+      {/* Tabs for Draft Management */}
+      <Tabs defaultValue={DraftListTab.writing} value={activeTab} onValueChange={(value) => setActiveTab(value)}>
+        <TabsList>
+          {DraftListTabs.map((tab) => (
+            <TabsTrigger key={tab.key} value={tab.key}>
+              {tab.label}
+              {tab.key === DraftListTab.scheduled && scheduledCastsCount > 0 ? (
+                <Badge variant="secondary" className="ml-2 text-xxs w-4 h-4 p-0 rounded-full">
+                  {scheduledCastsCount}
+                </Badge>
+              ) : null}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {DraftListTabs.map((tab) => (
+          <TabsContent key={tab.key} value={tab.key} className={cn('relative w-full overflow-hidden mt-2')}>
+            <ScrollArea className="h-full pb-24">
+              {draftsForTab.map((draft) => (
+                <div key={draft.id}>
+                  <div className="w-full">
+                    {renderWritingDraft(draft)}
+                    <div className="flex justify-end px-2 mb-2 mt-2">
+                      <Button onClick={() => onRemove(draft)} className="inline-flex items-center px-2 py-1">
+                        <TrashIcon className="w-4 h-4 text-muted-foreground inline mr-2" />
+                        Remove
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </ScrollArea>
+          </TabsContent>
+        ))}
+      </Tabs>
+      {/* Posts and Threads Management */}
+      <div>
+        {posts.map((post) => (
+          <div key={post.id} style={{ marginBottom: "20px" }}>
+            <textarea
+              placeholder="Enter your content"
+              value={post.content}
+              onChange={(e) => handleContentChange(e, post.id)}
+            />
+            <button onClick={() => addThread(post.id)}>
+              <img src={newThread} alt="New Thread" width="25" height="25" />
+            </button>
+            {post.threads.map((thread, index) => (
+              <textarea
+                key={index}
+                placeholder="Enter your thread content"
+                value={thread}
+                onChange={(e) => handleThreadContentChange(e, post.id, index)}
+              />
+            ))}
+          </div>
+        ))}
+        <button onClick={handlePostSubmit}>Submit</button>
       </div>
     </div>
   );
