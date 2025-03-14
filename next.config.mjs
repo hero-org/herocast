c2a8bf536c4c8c61b7566f6ba1c28316f32dcf86
hellno <hellno@users.noreply.github.com>
2025-03-14 11:32:46 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const nextConfig = {
-  unstable_allowDynamic: [
-    '@frames.js/render/dist/next/index.js',
-    'middleware.ts'
-  ],
+  unstable_allowDynamic: ['@frames.js/render/dist/next/index.js', 'middleware.ts'],
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
